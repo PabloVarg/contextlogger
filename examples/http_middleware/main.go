63ca252627c636bbd1b9a9d2923d63fa9b3e6840
Pablo <49590991+PabloVarg@users.noreply.github.com>
2025-10-30 20:27:34 -0600

    refactor(examples): Improve readability of http middleware

--- examples/http_middleware/main.go
+++ examples/http_middleware/main.go
@@ -15,18 +15,21 @@ func main() {
 }
 
 func runServer() {
-	handlerWithMiddleware := httpmiddleware.LoggingMiddleware(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			user := examples.DefaultUser()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := examples.DefaultUser()
 
-			contextlogger.UpdateContext(
-				contextlogger.WithGroup(r.Context(), "userInfo"),
-				"id", user.ID,
-				"email", user.Email,
-				"firstName", user.FirstName,
-			)
-		}),
-		httpmiddleware.WithLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil))),
+		contextlogger.UpdateContext(
+			contextlogger.WithGroup(r.Context(), "userInfo"),
+			"id", user.ID,
+			"email", user.Email,
+			"firstName", user.FirstName,
+		)
+	})
+
+	handlerWithMiddleware := httpmiddleware.LoggingMiddleware(
+		handler,
+		httpmiddleware.WithLogger(logger),
 	)
 
 	http.DefaultServeMux.Handle("GET /users/info", handlerWithMiddleware)
